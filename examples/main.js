0c453950233cbdedd4a2ec35d3676533253e1432
François Pluchino <francois.pluchino@gmail.com>
2018-06-07 09:56:52 +0200

    Replace grunt by webpack

---
+++ examples/main.js
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the Fxp package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import 'bootstrap/less/bootstrap.less';
+import 'font-awesome/less/font-awesome.less';
+import '@fxp/jquery-table-pager/less/table-pager.less';
+import '@fxp/jquery-table-pager/less/table-sort.less';
+import '../less/table-select.less';
+import '../js/table-select';
+import '@fxp/jquery-table-pager/js/locale/table-pager-fr';
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+/*
+ * This file is part of the Fxp package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const Encore = require('@symfony/webpack-encore');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = Encore
+    .setOutputPath('build/')
+    .setPublicPath('/build')
+    .autoProvidejQuery()
+    .enableSourceMaps(!Encore.isProduction())
+    .cleanupOutputBeforeBuild()
+    .enableLessLoader()
+    .addEntry('main', './examples/main.js')
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './examples', to: '.', test: /.html$/ }
+    ]))
+    .getWebpackConfig()
+;
+
+// Replace Webpack Uglify Plugin
+if (Encore.isProduction()) {
+    config.plugins = config.plugins.filter(
+        plugin => !(plugin instanceof webpack.optimize.UglifyJsPlugin)
+    );
+    config.plugins.push(new UglifyJsPlugin());
+}
+
+// Config of ajax data
+if (config.devServer) {
+    config.devServer.inline = true;
+    config.devServer.before = function (app) {
+        let url = require('url'),
+            querystring = require('querystring');
+
+        /**
+         * Generate the list.
+         *
+         * @param {Number} pn  The page number
+         * @param {Number} ps  The page size
+         * @param {Number} max The max size of the list
+         *
+         * @returns {Array}
+         */
+        function getRows(pn, ps, max) {
+            let rows = [],
+                start = (pn - 1) * ps,
+                end = 0 === ps ? max : Math.min(start + ps, max),
+                i;
+
+            for (i = start + 1; i <= end; i++) {
+                rows.push({
+                    '_row_number': i,
+                    '_row_id': i,
+                    'id': i.toString(),
+                    'firstname': 'First name ' + i,
+                    'lastname': 'Last name ' + i,
+                    'username': 'Username ' + i,
+                    '_selectable': '<input type="checkbox">'
+                });
+            }
+
+            return rows;
+        }
+
+        app.get("/ajax.json", function(req, res){
+            let params = querystring.parse(url.parse(req.url).query),
+                max = 70,
+                pn = 'pn' in params ? parseInt(params['pn']) : 1,
+                ps = 'ps' in params ? parseInt(params['ps']) : 10,
+                rows = getRows(pn, ps, max);
+
+            res.json({
+                size: max,
+                pageNumber: pn,
+                pageSize: ps,
+                rows: rows,
+                sortColumns: {}
+            });
+        });
+    };
+}
+
+module.exports = config;
